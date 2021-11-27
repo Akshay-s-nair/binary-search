5e8e5361c8f0ef5774ca9ee4c1e9760ba476d2d9
Akshay-s-nair <94843189+Akshay-s-nair@users.noreply.github.com>
2021-11-27 10:55:46 +0530

    Add files via upload

---
+++ main (4).c
@@ -0,0 +1,68 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+    int i,j,n,var,a[100],temp,left,right,mid;
+    printf("Enter the array limit: ");
+    scanf("%d", &n);
+    printf("Enter the array:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[j]<a[i])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("sorted array is:");
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+    do
+    {
+    printf("\n\nEnter the value to be searched: \n(Enter '0' to exit)\nvalue=");
+    scanf("%d",&var);
+    left=0;
+    right=n-1;
+    
+    while(left<=right)
+    {
+        mid=(left+right)/2;
+        if(a[mid]<var)
+        {
+            left=mid+1;
+            
+        }
+        else if(a[mid]>var)
+        {
+            right=mid-1;
+        }
+        else
+        {
+           printf("the element is in the %dth position",mid+1);
+           break;
+           
+        }
+       if(left>right)
+       printf("Element not found!!\n");
+    }
+    }while(var!=0);
+}
+
+
